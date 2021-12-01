0ada943a404d807447a443e7cd059a1d3e355b7c
Subi <asubigyan@gmail.com>
2021-12-01 15:25:51 +0545

    correct api port

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const connectDB = require("./config/db");
 
 app.use(express.json());
 app.use("/api", crudRouter);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
